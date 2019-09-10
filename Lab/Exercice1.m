e17fe13e315b59fe486ad6cc5957a0fe7e9ce8f9
glesssard <lesg2605@usherbrooke.ca>
2019-09-10 11:01:45 -0400

    Added plot to verify line

--- Lab/Exercice1.m
+++ Lab/Exercice1.m
@@ -3,10 +3,18 @@ clear
 clc
 
 %% The good stuff
-
+m=1;
+b=2;
 Xn = [1 3 4 6 7];
 Yn = [-1.6 4.8 6.1 14.6 15.1 ];
 m1 = inv([[5 sum(Xn)];[sum(Xn) sum(Xn.^2)]]);
 m2 = [sum(Yn); sum(Xn.*Yn)];
+rep = m1*m2
 
-rep = m1*m2;
+
+%% Plotting
+xp = linspace(0,10,100);
+yp = rep(2).*xp + rep(1);
+
+figure
+plot(xp,yp,Xn,Yn)
