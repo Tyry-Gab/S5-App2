350e21c51c02b95ad064387f654f4a3051723705
glesssard <lesg2605@usherbrooke.ca>
2019-09-11 16:09:04 -0400

    8b done

--- Lab/Exercice3.m
+++ Lab/Exercice3.m
@@ -12,16 +12,16 @@ papa_matrice = inv([
                 [sum(tn) sum(tn.^2) sum(tn.*cos(w.*tn)) sum(tn.*sin(w.*tn))];
                 [sum(cos(w.*tn)) sum(cos(w.*tn).*tn) sum(cos(w.*tn).^2) sum(cos(w.*tn).*sin(w.*tn))];
                 [sum(sin(w.*tn)) sum(sin(w.*tn).*tn) sum(sin(w.*tn).*cos(w.*tn)) sum(sin(w.*tn).^2)]
-               ])
-matrix_y = [sum(yn); sum(tn.*yn); sum(cos(w.*tn).*yn); sum(sin(w.*tn).*yn)]
+               ]);
+matrix_y = [sum(yn); sum(tn.*yn); sum(cos(w.*tn).*yn); sum(sin(w.*tn).*yn)];
 
-a = papa_matrice * matrix_y
+a = papa_matrice * matrix_y;
 
 t = linspace(0,15,1000);
 test = a(1) + a(2).*t + a(3).*cos(w.*t) + a(4).*sin(w.*t);
 res = 1.9998 - 0.1538.*t + 0.1074*cos(w.*t) + 0.3434*sin(w.*t);
 %% Ploting
-plot(tn,yn,t,res,t,test)
+%plot(tn,yn,t,res,t,test)
 
 %% RMS
 values = 1.9998 - 0.1538.*tn + 0.1074*cos(w.*tn) + 0.3434*sin(w.*tn);
@@ -29,9 +29,16 @@ papa_rms = sqrt(1/numel(res)*sum((values-yn).^2));
 
 y_barrre = 1/numel(tn)*sum(yn);
 
-R = sum((values-y_barrre).^2)/sum((yn-y_barrre).^2)
+R = sum((values-y_barrre).^2)/sum((yn-y_barrre).^2);
 
 
 %% More good stuff
 
+papa_matrice2 = [ones(1, numel(tn)); tn; cos(w.*tn); sin(w.*tn) ]';
 
+A = inv(papa_matrice2' * papa_matrice2) * papa_matrice2' * yn'
+test2 = A(1) + A(2).*t + A(3).*cos(w.*t) + A(4).*sin(w.*t);
+
+
+figure
+plot(tn,yn,t,test2)
